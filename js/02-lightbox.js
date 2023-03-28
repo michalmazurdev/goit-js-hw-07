3aad90d2db507004330b7f8595a19fda86ffbc15
michalmazurdev <m.mazur40@gmail.com>
2023-03-29 00:40:50 +0200

    js code  cleaned

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,17 +1,15 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
 const galleryEl = document.querySelector(".gallery");
-
 let markup = ``;
+
 for (const image of galleryItems) {
   markup += `<li> <a href=${image.original} class='gallery__item'> <img class='gallery__image' src=${image.preview} alt=${image.description}></a>
   </li>`;
 }
 galleryEl.innerHTML = markup;
+
 const lightbox = new SimpleLightbox(".gallery a", {
-  //   captionSelector: "img",
   captionType: "attr",
   captionsData: "alt",
   captionDelay: 250,
